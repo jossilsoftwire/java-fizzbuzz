7e43af201b218df3dc65fe221115daf26a1623d9
Joseph Silcock <Joseph.Silcock@softwire.com>
2020-11-17 22:57:04 +0000

    added user provided rules on/off

--- src/jossil/fizzbuzz/Main.java
+++ src/jossil/fizzbuzz/Main.java
@@ -9,24 +9,49 @@ public class Main {
         String countTo = sysReader.nextLine();
         int enteredInt = Integer.parseInt(countTo);
 
+        boolean threeOn, fiveOn, sevenOn, elevenOn, thirteenOn, seventeenOn;
+        System.out.println("Implement 3 rule...(y/n)");
+        String threeRule = sysReader.nextLine();
+        threeOn = threeRule.equals("y");
+
+        System.out.println("Implement 5 rule...(y/n)");
+        String fiveRule = sysReader.nextLine();
+        fiveOn = fiveRule.equals("y");
+
+        System.out.println("Implement 7 rule...(y/n)");
+        String sevenRule = sysReader.nextLine();
+        sevenOn = sevenRule.equals("y");
+
+        System.out.println("Implement 11 rule...(y/n)");
+        String elevenRule = sysReader.nextLine();
+        elevenOn = elevenRule.equals("y");
+
+        System.out.println("Implement 13 rule...(y/n)");
+        String thirteenRule = sysReader.nextLine();
+        thirteenOn = thirteenRule.equals("y");
+
+        System.out.println("Implement 17 rule...(y/n)");
+        String seventeenRule = sysReader.nextLine();
+        seventeenOn = seventeenRule.equals("y");
+
         for (int i = 1; i < enteredInt + 1; i++) {
             String toPrint = "";
             if (i % 3 == 0) {
-                if (i % 5 == 0) {
+                if (i % 5 == 0 && threeOn && fiveOn) {
                     toPrint = "FizzBuzz";
-                } else {
+                } else if (threeOn) {
                     toPrint = "Fizz";
                 }
-            } else if (i % 5 == 0) {
+            } else if (i % 5 == 0 && fiveOn) {
                 toPrint = "Buzz";
             }
-            if (i % 7 == 0) {
+            if (i % 7 == 0 && sevenOn) {
                 toPrint += "Bang";
             }
-            if (i % 11 == 0) {
+            if (i % 11 == 0 && elevenOn) {
                 toPrint = "Bong";
             }
-            if (i % 13 == 0) {
+            if (i % 13 == 0 && thirteenOn) {
                 boolean changed = false;
                 for (int j = 0; j < toPrint.length() / 4; j++) {
                     if (toPrint.charAt(4 * j) == 'B') {
@@ -43,7 +68,7 @@ public class Main {
                     toPrint += "Fezz";
                 }
             }
-            if (i % 17 == 0) {
+            if (i % 17 == 0 && seventeenOn) {
                 if (toPrint.length() > 4) {
                     String newToPrint = "";
                     for (int j = toPrint.length() / 4; j > 0; j--) {
