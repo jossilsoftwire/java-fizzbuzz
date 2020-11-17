4519a983b48f1a7046fa7a8ff016afe128e2bf3e
Joseph Silcock <Joseph.Silcock@softwire.com>
2020-11-17 22:55:09 +0000

    added support for 13

--- src/jossil/fizzbuzz/Main.java
+++ src/jossil/fizzbuzz/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100 + 1; i++) {
+        for (int i = 1; i < 100; i++) {
             String toPrint = "";
             if (i % 3 == 0) {
                 if (i % 5 == 0) {
@@ -20,6 +20,23 @@ public class Main {
             }
             if (i % 11 == 0) {
                 toPrint = "Bong";
+            }
+            if (i % 13 == 0) {
+                boolean changed = false;
+                for (int j = 0; j < toPrint.length() / 4; j++) {
+                    if (toPrint.charAt(4 * j) == 'B') {
+                        if (j == 0) {
+                            toPrint = "Fezz" + toPrint.substring(0, 4);
+                        } else {
+                            toPrint = toPrint.substring(4 * (j - 1), 4 * j) + "Fezz" + toPrint.substring(4 * j, 4 * (j + 1));
+                        }
+                        changed = true;
+                        break;
+                    }
+                }
+                if (!changed) {
+                    toPrint += "Fezz";
+                }
             }
             if (toPrint.length() == 0) {
                 toPrint = Integer.toString(i);
